a37164357aab725a8e40d44be369ffaa262a5531
Anatole HAGBE <ahagbes89@gmail.com>
2026-08-06 16:57:07 +0100

    Added health check endpoint (#2)

---
+++ test_api.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from api import app
+
+client = TestClient(app)
+
+def test_healthcheck():
+    response = client.get("/healthz")
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "OK"}
